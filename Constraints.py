c73ea4536e2bff9501c0bc7f22191ed1935f3574
Noy <noystll@gmail.com>
2018-07-10 19:54:06 +0300

    Small fixes on  constraints

--- Constraints.py
+++ Constraints.py
@@ -13,13 +13,14 @@ class Constraints:
         :param preferences: A list of the workers preferences. For example:
          [(1,2,3)] is a list with one preference: worker1 wants to work on
          shift 3 on Tuesday.
-        :param variable_names: A list of variables names
+        :param variable_names: A list of variables names. A variable name for
+        example would be "(1,2,3)"
         """
         self.__preferences = preferences
-        self.__variables = variable_names
-        self.__all_constraints = {}  # (Variable names: constraints on variables)
-        self.__visible_constraints = {}  # (Variable names: constraints on variables)
-        self.__constraints_by_var = {}  # (var name: constrains on var)
+        self.__variable_names = variable_names
+        self.__all_constraints = {}  # (Variable names): [constraints on variables]
+        self.__visible_constraints = {}  # (Variable names): [constraints on variables]
+        self.__constraints_by_var = {}  # (var name): [constrains on var]
         self.__build_all_constraints()
 
     ###################
@@ -43,11 +44,11 @@ class Constraints:
         """
         Generates the soft constraints and updates self.constraints.
         """
-        for preference in self.preferences:
+        for preference in self.__preferences:
             var_name = preference
             # Adding constraint to all_constraints:
-            new_constraint = Constraint(var_name, 1, 1)
-            self.all_constraints[[var_name]] = [new_constraint]
+            new_constraint = Constraint(var_name, [(True)], 1)
+            self.__all_constraints[(var_name)] = [new_constraint]
 
     def __set_constraint_by_var(self):
         pass
@@ -58,13 +59,19 @@ class Constraints:
 
     def get_visible_constraints(self):
         pass
-    def __set_visible_constrains(self):
+
+    def get_all_constraints(self):
+        return self.__all_constraints
+
+    def set_visible_constrains(self):
         pass
+
 
 #########
 # Tests #
 #########
-p = [(1,2,3)]
-vars = [(1,2,3)]
+p = [(1, 2, 3), (4, 5, 6)]
+vars = [(1, 2, 3), (4, 5, 6)]
 
-c = Constraints(p,vars)
+c = Constraints(p, vars)
+print(c.get_all_constraints())
