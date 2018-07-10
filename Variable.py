4258c67868cdc95d3aff049d9fc281921596e603
Jonathanweiss <jonathan.weiss1@mail.huji.ac.il>
2018-07-10 20:43:21 +0300

    Csp now initializes variable instances. minimal constraint and variable skelatons updated.

--- Variable.py
+++ Variable.py
@@ -3,19 +3,27 @@ class Variable:
     Represent variable and saves relevant information inside it.
     """
 
-    def __init__(self, name, domain_values):
+    def __init__(self, name, domain_values, constraints):
         """
         :param name: name of the variable
         :param domain_values: a frozenSet containing possible values. values should be imutable.
+        :param constraints: list of constraints objects.
         """
         # basic attributes:
         self.name = name
         self.domain = domain_values
-        self.possible_domain = domain_values
-        self.neighboursNames = []  # TODO add them.
+        self.constraints = constraints
+        self.possible_domain = self.domain  # will get smaller or larger in time.
+
         # forward checking relevant attributes:
         self.affected_variables = []  # remembers the variables that were affected by forward checking.
         self.affecting_value = None  # remember the value to return in the end of forward checking restoration.
+
+        self.neighboursNames = set()
+
+    def add_neighbours(self, set_of_neighbours):
+        for neighbour in set_of_neighbours:
+            self.neighboursNames.add(neighbour)
 
     def forwad_checking_restore_self(self):
         """
@@ -24,3 +32,7 @@ class Variable:
         """
         self.affected_variables = []
         self.affecting_value = None
+
+    def check_value_assignment(self, value):
+        # TODO : should check all constraints if this value is legit. (intersection of consrtaints).
+        pass
