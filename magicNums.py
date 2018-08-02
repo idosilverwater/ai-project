5c5da3016a8062abc0a63163f73812ad36719ff7
Noy <noystll@gmail.com>
2018-08-02 15:30:34 +0300

    Finished FormulaTree

--- magicNums.py
+++ magicNums.py
@@ -9,3 +9,7 @@ DOMAIN_FALSE_VAL = 'False'
 # Days and shifts for the bakery problem.
 DAYS_IN_WEEK = 3  # number of days in a week.
 SHIFTS_IN_DAY = 2  # number of shifts in a day.
+
+# Boolean
+AND = "&"
+OR = "|"
